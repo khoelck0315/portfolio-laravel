6a8bcf75c64b1b134cdd0ea09657fb04fb2f8b0c
Kevin Hoelck <khoelck0315@gmail.com>
2023-09-19 09:26:13 -0400

    Update vite.config, app.scss for production builds

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,12 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+		        'resources/js/bootstrap.js',
+		        'resources/js/home.js',
+		        'resources/js/custom/scrolltracking.js',
                 'resources/js/app.js',
+		        'resources/css/home.css',
+                'resources/sass/app.scss',
             ],
             refresh: true,
             
