732634817d3214868f2c4662dba6bb8b160a9040
Kevin Hoelck <khoelck0315@gmail.com>
2023-09-21 11:12:01 -0400

    Add react to project

---
+++ app/Http/Controllers/UtilityController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UtilityController extends Controller
+{
+    //TODO markdown editor
+    //TODO cron job editor
+    //TODO conversions (unix timestamp, base64, ...)
+    //TODO subnet calculator
+}
